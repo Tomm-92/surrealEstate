f8237f944d6d77a5a376eb8e60707c09b3fb5c6f
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-04-20 18:55:48 +0100

    property cards rendering from API

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,6 @@ import {
 import NavBar from "./NavBar";
 import Properties from "./Properties";
 import AddProperty from "./AddProperty";
-import PropertyCard from "./PropertyCard";
 
 const App = () => {
   return (
@@ -19,23 +18,7 @@ const App = () => {
       <div className="App">
         <NavBar />
         <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Properties />
-                <PropertyCard
-                  title=" 6 bedroom family property"
-                  type="House"
-                  bathrooms="3"
-                  bedrooms="6"
-                  price="£500,000"
-                  city="Manchester"
-                  email="tom@gmail.com"
-                />
-              </>
-            }
-          />
+          <Route path="/" element={<Properties />} />
           <Route path="/add-property" element={<AddProperty />} />
         </Routes>
       </div>
--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Alert from "./Alert";
+import PropertyCard from "./PropertyCard";
+import "../styles/properties.css";
 
 const Properties = () => {
-  return <div> Property Summary </div>;
+  const [properties, setProperties] = useState([]);
+  const [alert, setAlert] = useState({ message: "" });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/v1/PropertyListing")
+      .then(({ data }) => {
+        setProperties(data);
+      })
+      .catch(() =>
+        setAlert({
+          message: "Server error. Please come back later =(",
+        })
+      );
+  }, []);
+  return (
+    <div className="properties">
+      <Alert message={alert.message} />
+      {properties.map((response) => (
+        <div key={response._id} className="item">
+          <PropertyCard {...response} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Properties;
