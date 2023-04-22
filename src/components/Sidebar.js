8645906f08fa51bd6294e1f27ffe2870a91b374d
Tom Wilkinson <thomasmarkwilkinson@gmail.com>
2023-04-22 14:52:36 +0100

    search by title added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import qs from "qs";
 import "../styles/sidebar.css";
 
 const Sidebar = () => {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { search } = useLocation();
+
+  const buildQueryString = (operation, valueObj) => {
+    const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
+
+    const newQueryParams = {
+      ...currentQueryParams,
+      [operation]: JSON.stringify({
+        ...JSON.parse(currentQueryParams[operation] || "{}"),
+        ...valueObj,
+      }),
+    };
+    return qs.stringify(newQueryParams, {
+      addQueryPrefix: true,
+      encode: false,
+    });
+  };
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    const newQueryString = buildQueryString("query", {
+      title: { $regex: query },
+    });
+    navigate(newQueryString);
+  };
   return (
     <div className="sidebar">
       {" "}
@@ -10,26 +38,53 @@ const Sidebar = () => {
       <ul className="sidebar-links">
         <Link
           className="sidebar-links-item"
-          to={`/?query={"city": "Liverpool"}`}
+          to={buildQueryString("sort", { price: 1 })}
+        >
+          Sort - Ascending
+        </Link>
+        <Link
+          className="sidebar-links-item"
+          to={buildQueryString("sort", { price: -1 })}
+        >
+          Sort - Descending
+        </Link>
+        <Link
+          className="sidebar-links-item"
+          to={buildQueryString("query", { city: "Liverpool" })}
         >
           Liverpool
         </Link>
         <Link
           className="sidebar-links-item"
-          to={`/?query={"city": "Manchester"}`}
+          to={buildQueryString("query", { city: "Manchester" })}
         >
           Manchester
         </Link>
         <Link
           className="sidebar-links-item"
-          to={`/?query={"city": "Sheffield"}`}
+          to={buildQueryString("query", { city: "Sheffield" })}
         >
           Sheffield
         </Link>
-        <Link className="sidebar-links-item" to={`/?query={"city": "Leeds"}`}>
+        <Link
+          className="sidebar-links-item"
+          to={buildQueryString("query", { city: "Leeds" })}
+        >
           Leeds
         </Link>
       </ul>
+      <form className="search-form" onSubmit={handleSearch}>
+        <input
+          className="search-input"
+          type="text"
+          value={query}
+          placeholder="Search for a property"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="search-form__submit-button">
+          Search!
+        </button>
+      </form>
     </div>
   );
 };
